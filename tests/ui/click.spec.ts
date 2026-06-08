fac68b498baedb7d517b1cccbf8109fb00347798
Netwear7 <59568212+Netwear7@users.noreply.github.com>
2026-06-08 14:24:35 +0200

    feat: enhance ClickPage with WebKit support and update tests (#2)

--- tests/ui/click.spec.ts
+++ tests/ui/click.spec.ts
@@ -1,10 +1,12 @@
 import { test } from '@playwright/test';
 import { ClickPage } from '../../pages/click.page';
 
-test('basic click test', async ({ page }) => {
+// This page ignores event-based click actions, so we use a physical mouse click
+// emulation to make the scenario work in WebKit as well.
+test('basic click test', async ({ page, browserName }) => {
     const clickPage = new ClickPage(page);
     await clickPage.goto();
     await clickPage.expectButtonState('Button That Ignores DOM Click Event', "btn-primary");
-    await clickPage.clickButton();
+    await clickPage.clickButton(browserName);
     await clickPage.expectButtonState('Button That Ignores DOM Click Event', "btn-success"); // Vérifie que le texte et la couleur du bouton ont changé
 });
